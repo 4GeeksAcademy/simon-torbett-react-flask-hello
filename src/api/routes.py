3a74454e2950360548ac3c306de0b9d908623915
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2024-01-08 22:58:01 +0000

    first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,19 +4,53 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+import json
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
-CORS(api)
 
+@api.route('/register', methods=['POST'])
+def create_user():    
+    body = json.loads(request.data)    
+    user = User(email = body["email"], password= body["password"], is_active = True)    
+    db.session.add(user)    
+    db.session.commit()
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    response_body = {  "msg": " The new user has been created correctly "    }
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
     return jsonify(response_body), 200
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)    
+    password = request.json.get("password", None)    
+    user = User.query.filter(User.email == email, User.password == password, User.is_active == True).first()    
+    print(email,password)   
+    if not user :      
+          return jsonify({"msg": "Bad username or password"}), 401
+    else:           
+         access_token = create_access_token(identity=email)     
+         response_body = {
+            "email": email,                  
+            "access_token": access_token}      
+         return jsonify(response_body), 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == "__main__":
+    app.run()
+    
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,96 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+import {
+    Register
+} from "../pages/register";
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            token: null,
+        },
+        actions: {
+            register: async (email, password) => {
+                const options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                    }),
+                };
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+                try {
+                    const resp = await fetch(
+                        "https://3001-spaganodova-reactflaskh-27xaudxerdk.ws-eu96.gitpod.io/api/register",
+                        options
+                    );
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                    if (resp.status !== 200) {
+
+                        alert("An error has occurred while creating the user");
+                        return false;
+                    }
+
+                    const data = await resp.json();
+                    console.log(data);
+                    return true;
+                } catch (error) {
+                    console.error("error");
+                    console.log(error);
+                }
+            },
+
+            login: async (email, password) => {
+                const options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                    }),
+                };
+
+                try {
+                    const resp = await fetch(
+                        "https://3001-spaganodova-reactflaskh-27xaudxerdk.ws-eu96.gitpod.io/api/login",
+                        options
+                    );
+                    console.log(options.headers)
+                    if (resp.status !== 200) {
+
+                        alert("no ingreso");
+                        return false;
+                    }
+
+                    const data = await resp.json();
+                    localStorage.setItem("token", data.access_token.token);
+                    setStore({
+                        token: data.access_token.token,
+
+                    });
+                    return true;
+
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+
+            syncToken: () => {
+                const token = localStorage.getItem("token");
+                if (token && token != "" && token != undefined)
+                    setStore({
+                        token: token,
+                    });
+            },
+        }
+    };
 };
 
 export default getState;
